4a91c742caa7036bd39349cfc0815f206e8fee0f
Rekhansh Priyadarshi <priyadarshirekhansh@gmail.com>
2019-09-21 16:19:56 +0530

    Whp Am I

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,13 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const { dialogflow } = require('actions-on-google')
 
+const app = dialogflow()
+
+app.intent('Who am I', conv => {
+  conv.ask(JSON.stringify(conv.user))
+})
+
+exports.collegeMate = functions.https.onRequest(app)
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
